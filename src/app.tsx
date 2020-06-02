b6efba0b0eb75ad3d7918d982fbee7a3de485324
Matthias Schott <matthias.schott@gmail.com>
2020-06-02 14:52:58 +0200

    Establish osc connection between Electron and Ardour (handle incoming messages, etc.) Create temp folder to give feedback to Ardour (Ardour can only send out osc msgs) FIX: app can now receive a title as a prop FIX: change fill-rule property of the svg path to JSX format

--- src/app.tsx
+++ src/app.tsx
@@ -4,14 +4,58 @@ import './styles/bootstrap.min.css';
 /* JS libs imports */
 import React from "react";
 import ReactDOM from "react-dom";
+import fs from "fs";
+import os from "os";
+import udp from "dgram";
+import osc from "osc-min";
 
 interface MelodyhelprProps {
-    title: string,
+    title?: string,
 }
 
-class Melodyhelpr extends React.Component {
+const PORT = 7890;
+const HOME_DIR = os.homedir();
+const TEMP_DIR = HOME_DIR.concat("/ardour_electron");
+const CONN_FILE = "/connected.txt";
+
+class Melodyhelpr extends React.Component<MelodyhelprProps> {
+    
     constructor(props: MelodyhelprProps) {
         super(props);
+        if (props.title) document.title = props.title;
+        this.openSocket = this.openSocket.bind(this);
+    }
+
+    componentDidMount() {
+        this.openSocket();
+    }
+
+    openSocket() {
+        const sock = udp.createSocket("udp4", (msg, rinfo) => {
+            let error, error1;
+            try {
+                // handle messages sent from Ardour (destructering of incoming msgs)
+                if (osc.fromBuffer(msg)["address"] === "SEQ_INFO") {
+                    
+                    // check if boolean flag (exp_chords) in Ardour has been set
+                    if (osc.fromBuffer(msg)["args"][3].value === true) {  
+                    }
+                    
+                }
+                // establish connection with Ardour (basic fs check for now)
+                if (osc.fromBuffer(msg)["address"] === "CONNECT") {
+                    fs.writeFile(TEMP_DIR.concat(CONN_FILE), "connect", "utf8", err => {
+                        if (err) throw err;
+                        console.log("The file has been written!");
+                    });
+                }
+                return console.log(osc.fromBuffer(msg));
+            } catch (error1) {
+                error = error1;
+                return console.log("Error: ", error);
+            }
+        });
+        sock.bind(PORT);
     }
 
     render() {
@@ -46,11 +90,13 @@ class Melodyhelpr extends React.Component {
                                 <div className="footer-right d-flex">
                                     <button className="btn btn-outline-secondary">:2</button>
                                     <button className="btn btn-outline-secondary">x2</button>
-                                    <button className="btn btn-outline-secondary"><svg className="bi bi-shuffle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M12.646 1.146a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.793 4l-2.147-2.146a.5.5 0 0 1 0-.708zm0 8a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.793 12l-2.147-2.146a.5.5 0 0 1 0-.708z" />
-                                        <path fill-rule="evenodd" d="M0 4a.5.5 0 0 1 .5-.5h2c3.053 0 4.564 2.258 5.856 4.226l.08.123c.636.97 1.224 1.865 1.932 2.539.718.682 1.538 1.112 2.632 1.112h2a.5.5 0 0 1 0 1h-2c-1.406 0-2.461-.57-3.321-1.388-.795-.755-1.441-1.742-2.055-2.679l-.105-.159C6.186 6.242 4.947 4.5 2.5 4.5h-2A.5.5 0 0 1 0 4z" />
-                                        <path fill-rule="evenodd" d="M0 12a.5.5 0 0 0 .5.5h2c3.053 0 4.564-2.258 5.856-4.226l.08-.123c.636-.97 1.224-1.865 1.932-2.539C11.086 4.93 11.906 4.5 13 4.5h2a.5.5 0 0 0 0-1h-2c-1.406 0-2.461.57-3.321 1.388-.795.755-1.441 1.742-2.055 2.679l-.105.159C6.186 9.758 4.947 11.5 2.5 11.5h-2a.5.5 0 0 0-.5.5z" />
-                                    </svg></button>
+                                    <button className="btn btn-outline-secondary">
+                                        <svg className="bi bi-shuffle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                          <path fillRule="evenodd" d="M12.646 1.146a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.793 4l-2.147-2.146a.5.5 0 0 1 0-.708zm0 8a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.793 12l-2.147-2.146a.5.5 0 0 1 0-.708z" />
+                                          <path fillRule="evenodd" d="M0 4a.5.5 0 0 1 .5-.5h2c3.053 0 4.564 2.258 5.856 4.226l.08.123c.636.97 1.224 1.865 1.932 2.539.718.682 1.538 1.112 2.632 1.112h2a.5.5 0 0 1 0 1h-2c-1.406 0-2.461-.57-3.321-1.388-.795-.755-1.441-1.742-2.055-2.679l-.105-.159C6.186 6.242 4.947 4.5 2.5 4.5h-2A.5.5 0 0 1 0 4z" />
+                                          <path fillRule="evenodd" d="M0 12a.5.5 0 0 0 .5.5h2c3.053 0 4.564-2.258 5.856-4.226l.08-.123c.636-.97 1.224-1.865 1.932-2.539C11.086 4.93 11.906 4.5 13 4.5h2a.5.5 0 0 0 0-1h-2c-1.406 0-2.461.57-3.321 1.388-.795.755-1.441 1.742-2.055 2.679l-.105.159C6.186 9.758 4.947 11.5 2.5 11.5h-2a.5.5 0 0 0-.5.5z" />
+                                        </svg>
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -61,4 +107,4 @@ class Melodyhelpr extends React.Component {
     }
 }
 
-ReactDOM.render(<Melodyhelpr />, document.getElementById("my-app"));
+ReactDOM.render(<Melodyhelpr title="Melody Helpr" />, document.getElementById("my-app"));
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
+// imports
+import fs from 'fs';
+import os from 'os';
+// usr vars
+const HOME_DIR = os.homedir();
+const TEMP_DIR = HOME_DIR.concat('/ardour_electron');
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
+}
+
+const createDir = () => {
+  // Creates a temp folder, used to communicate with Ardour
+  try {
+    fs.mkdirSync(TEMP_DIR, { recursive: true });
+  } catch (err) {
+    console.log('error code: ', err.code);
+    if (err.code === 'EEXIST') {
+      console.error('Directory already exists');
+    }
+  }
+}
+
+const removeDir = () => {
+  // Remove temp folder, but first delete files in folder
+  if (fs.existsSync(TEMP_DIR)) {
+    fs.readdirSync(TEMP_DIR).forEach((file) => {
+      const curPath = TEMP_DIR.concat('/', file);
+      fs.unlinkSync(curPath);
+    });
+    fs.rmdirSync(TEMP_DIR);
+  }
 }
 
 const createWindow = () => {
@@ -21,6 +51,9 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  // create tmp folder -> acts as an exchange folder between this app and Ardour
+  createDir();
 };
 
 // This method will be called when Electron has finished
@@ -30,6 +63,8 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
+  // remove tmp folder
+  removeDir();
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
