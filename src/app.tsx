788241715849bd5208a7318634fd583fc89c8064
Matthias Schott <matthias.schott@gmail.com>
2020-06-06 14:46:04 +0200

    Add function to create a new sequence (with current settings) Add input box to change the temperature (randomness) Change CSS to reflect changes and do some minor adjustments

--- src/app.tsx
+++ src/app.tsx
@@ -61,6 +61,7 @@ class Melodyhelpr extends React.Component<MelodyhelprProps, MelodyhelprState> {
         this.model = undefined;
         this.openSocket = this.openSocket.bind(this);
         this.generateSequence = this.generateSequence.bind(this);
+        this.changeTemp = this.changeTemp.bind(this);
         this.doubleSequence = this.doubleSequence.bind(this);
         this.halveSequence = this.halveSequence.bind(this);
         this.transferToArdour = this.transferToArdour.bind(this);
@@ -141,6 +142,12 @@ class Melodyhelpr extends React.Component<MelodyhelprProps, MelodyhelprState> {
         // this.model.dispose(); // TODO: check at which point model should be disposed
     }
 
+    changeTemp(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            temperature: Number.parseFloat(event.target.value)
+        });
+    }
+
     async doubleSequence() {
         const outputLength = this.state.bars * this.state.stepsPerBar;
 
@@ -209,10 +216,13 @@ class Melodyhelpr extends React.Component<MelodyhelprProps, MelodyhelprState> {
                             <div className='card-title'> 
                             </div>
                             <div className='card-body'>
-                                <p id='status-bar'>
-                                {`bpm: ${this.state.qpm.toFixed(1)} | time: ${this.state.divisions}/${this.state.divisor} | bars: ${this.state.bars}  
-                                | randomness: ${this.state.temperature}`}
-                        </p>
+                                <div id='status-bar' className='d-flex justify-content-center'>
+                                    <p>
+                                    {`bpm: ${this.state.qpm.toFixed(1)} | time: ${this.state.divisions}/${this.state.divisor} | bars: ${this.state.bars}  
+                                    | randomness: `}
+                                    </p>
+                                    <input id='temp-input' type='number' step={0.1} min={0.5} max={2.0} value={this.state.temperature.toFixed(1)} onChange={this.changeTemp}></input>
+                                </div>
                                 <div id='note-player'>
                                 <NotePlayer
                                     play={this.state.notesCanBePlayed}
@@ -224,20 +234,18 @@ class Melodyhelpr extends React.Component<MelodyhelprProps, MelodyhelprState> {
 
                                 </div>
                             </div>
-                            <div className='card-footer d-flex'>
-                                <div className='footer-left'>
-                                    <button className='btn btn-outline-secondary' onClick={this.transferToArdour}>Transfer</button>
-                                </div>
-                                <div className='footer-right d-flex'>
-                                    <button className='btn btn-outline-secondary' disabled={this.state.bars === 2} onClick={this.halveSequence}>:2</button>
-                                    <button className='btn btn-outline-secondary' disabled={this.state.bars === 8} onClick={this.doubleSequence}>x2</button>
-                                    <button className='btn btn-outline-secondary'>
-                                        <svg className='bi bi-shuffle' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
-                                          <path fillRule='evenodd' d='M12.646 1.146a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.793 4l-2.147-2.146a.5.5 0 0 1 0-.708zm0 8a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.793 12l-2.147-2.146a.5.5 0 0 1 0-.708z' />
-                                          <path fillRule='evenodd' d='M0 4a.5.5 0 0 1 .5-.5h2c3.053 0 4.564 2.258 5.856 4.226l.08.123c.636.97 1.224 1.865 1.932 2.539.718.682 1.538 1.112 2.632 1.112h2a.5.5 0 0 1 0 1h-2c-1.406 0-2.461-.57-3.321-1.388-.795-.755-1.441-1.742-2.055-2.679l-.105-.159C6.186 6.242 4.947 4.5 2.5 4.5h-2A.5.5 0 0 1 0 4z' />
-                                          <path fillRule='evenodd' d='M0 12a.5.5 0 0 0 .5.5h2c3.053 0 4.564-2.258 5.856-4.226l.08-.123c.636-.97 1.224-1.865 1.932-2.539C11.086 4.93 11.906 4.5 13 4.5h2a.5.5 0 0 0 0-1h-2c-1.406 0-2.461.57-3.321 1.388-.795.755-1.441 1.742-2.055 2.679l-.105.159C6.186 9.758 4.947 11.5 2.5 11.5h-2a.5.5 0 0 0-.5.5z' />
+                            <div id='button-area' className='card-footer d-flex justify-content-between'>
+                                <div className='footer-left d-flex'>
+                                <button className='btn btn-outline-secondary' onClick={this.generateSequence}>
+                                        <svg id='lightning-icon' className='bi bi-lightning-fill' width='1.5em' height='1.5em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+                                            <path fillRule='evenodd' d='M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z'/>
                                         </svg>
                                     </button>
+                                    <button className='btn btn-outline-secondary' disabled={this.state.bars === 2} onClick={this.halveSequence}>:2</button>
+                                    <button className='btn btn-outline-secondary' disabled={this.state.bars === 8} onClick={this.doubleSequence}>x2</button>
+                                </div>
+                                <div className='footer-right d-flex'>
+                                    <button className='btn btn-outline-secondary' onClick={this.transferToArdour}>Transfer</button> 
                                 </div>
                             </div>
                         </div>
